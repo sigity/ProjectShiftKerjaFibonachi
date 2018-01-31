66b03c39d14d2570e85d2094b582e2a516caa8c2
nathanchristiawan <35796785+nathanchristiawan@users.noreply.github.com>
2018-01-31 14:38:09 +0700

    comment

--- src/projectshiftkerjafibonachi/Pekerja.java
+++ src/projectshiftkerjafibonachi/Pekerja.java
@@ -18,6 +18,12 @@ public class Pekerja extends Hari{
     private int i = 0;
     String NM [] ={"AMANDA","BRAM","CHARLES","DONNY","DISTA","ARISTON","TAUFIK","YUSRI","ADITYA","BAGAS"};
     
+    /**
+     * method untuk mengolah jumlah pekerja dan list nama pekerja
+     * @param jmlpekerja bernilai Integer untuk menyimpan nilai jumlah pekerja
+     * @return mengambil nilai dari List namapekerja
+     * @throws CustomException 
+     */
     public List HitungPekerja(int jmlpekerja) throws CustomException{
         List NamaPekerja = new ArrayList();
             while (i < jmlpekerja) {            
@@ -27,7 +33,15 @@ public class Pekerja extends Hari{
         return NamaPekerja;
     }
     
-    public String print(List<String> nm, List<Integer> hr , int jmlpekerja , javax.swing.JTable jTable){
+    /**
+     * method untuk menampilkan data penjadwalan kerja yang telah diolah
+     * @param nm bernilai List<String> untuk menampilkan data namapekerja
+     * @param hr bernilai List<integer> untuk menampilkan list hari berdasarkan jumlah hari
+     * @param jmlpekerja bernilai Integer untuk menyimpan nilai jumlah pekerja
+     * @param jTable digunakan untuk menampillkan seluruh data yang dipanggil dalam bentuk kolom dan baris (tabel)
+     * @return 
+     */
+    public void print(List<String> nm, List<Integer> hr , int jmlpekerja , javax.swing.JTable jTable){
          int x = 0, y = 0;
          List<Object> v = new ArrayList();
          for (int k = 0 , l = 0 ; k < hr.size(); k++, l++) {
@@ -53,7 +67,7 @@ public class Pekerja extends Hari{
              oModel.addRow((Object[])o);
          }
         jTable.setModel(oModel);
-        return v.toString();
+//        return v.toString();
      }
     
 
